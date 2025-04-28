9ef22b2a5f23d6f2e789344677f8ae0fb1c3ee80
MercedesSenties <anamercedes.senties@gmail.com>
2025-04-28 17:14:00 -0600

    feat: added metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import UseScrollToTop from "@/hooks/useScrollToTop";
 import { appWithTranslation, useTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
 import { Raleway } from "next/font/google";
+import Head from "next/head";
 
 const raleway = Raleway({
   subsets: ["latin"],
@@ -20,6 +21,19 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <div className={`transition duration-300 ${raleway.variable}`}>
+      <Head>
+        <title>Ana Mercedes Senties</title>
+        <meta name="description" content={t("banner.subtitle")} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Ana Mercedes Senties" />
+        <meta property="og:description" content={t("banner.subtitle")} />
+        <meta
+          property="og:image"
+          content="https://anamercedes.dev/images/logo.png"
+        />
+      </Head>
+
       <AppHeader />
       <Component {...pageProps} />
       <AppFooterCopyright t={t} />
